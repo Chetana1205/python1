91052b811b4dd3e24f2b2c069afdb8e99ac03365
Chetana1205 <101184515+Chetana1205@users.noreply.github.com>
2023-04-25 15:16:04 +0530

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 21 10:56:33 2022
+
+@author: Admin
+"""
+a = int(input("Enter a? "));  
+b = int(input("Enter b? "));  
+c = int(input("Enter c? "));  
+if a>b and a>c:  
+    print("a is largest");  
+if b>a and b>c:  
+    print("b is largest");  
+if c>a and c>b:  
+    print("c is largest");  
